28bd47442341a25188593ee0c71cb4282b5085b8
Jon Pither <jon.pither@gmail.com>
2014-05-27 07:34:51 +0100

    Split out cider-spy-msg.el

---
+++ cider-spy-msg.el
@@ -0,0 +1,50 @@
+;;; cider-spy-msg.el --- Send messages through CIDER SPY.
+
+;; Copyright © 2014 Jon Pither
+
+;; Author: Jon Pither <jon.pither@gmail.com>
+;; URL: http://www.github.com/jonpither/cider-spy
+;; Version: 0.1.0
+;; Keywords: languages, clojure, cider, nrepl
+;; Package-Requires: ((cider "0.5.0"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+(defun cider-spy-edit-message ()
+  (interactive)
+
+  (let ((buf (get-buffer-create "*cider spy msg*")))
+    (pop-to-buffer buf)
+    (cider-spy-edit-mode)
+    (message "Type C-c C-c to send (C-c C-k to cancel).")))
+
+(defun cider-spy-send-foo ()
+  (interactive)
+  (message "sent")
+  (kill-buffer (get-buffer "*cider spy msg*")))
+
+(defvar cider-spy-edit-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") 'cider-spy-send-foo)
+;;    (define-key map (kbd "C-c C-k") 'cider-spy-send-foo)
+    map
+    ))
+
+(define-derived-mode cider-spy-edit-mode text-mode "Cider Spy Edit")
+
+(provide 'cider-spy-msg)
+
+;;; cider-spy.el ends here
--- cider-spy.el
+++ cider-spy.el
@@ -24,6 +24,7 @@
 ;; This file is not part of GNU Emacs.
 
 (require 'cider-interaction)
+(require 'cider-spy-msg)
 (require 'json)
 (require 'dash)
 
@@ -328,30 +329,6 @@ the current buffer will be updated accordingly."
            "alias" alias)
      nil)))
 
-(defun cider-spy-send-message ()
-  (interactive)
-
-  (let ((buf (get-buffer-create "*cider spy msg*")))
-    (pop-to-buffer buf)
-    (cider-spy-edit-mode)
-    (message "Type C-c C-c to send (C-c C-k to cancel).")))
-
-;; todo split out the "send-mode"
-
-(defun cider-spy-send-foo ()
-  (interactive)
-  (message "sent")
-  (kill-buffer (get-buffer "*cider spy msg*")))
-
-(defvar cider-spy-edit-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-c") 'cider-spy-send-foo)
-;;    (define-key map (kbd "C-c C-k") 'cider-spy-send-foo)
-    map
-    ))
-
-(define-derived-mode cider-spy-edit-mode text-mode "Cider Spy Edit")
-
 (defvar cider-spy-buffer-mode-map
   (let ((map (make-sparse-keymap)))
     (suppress-keymap map t)
@@ -360,6 +337,7 @@ the current buffer will be updated accordingly."
     (define-key map (kbd "n") 'cider-spy-next-section)
     (define-key map (kbd "p") 'cider-spy-previous-section)
     (define-key map (kbd "a") 'cider-spy-alias)
+    (define-key map (kbd "s") 'cider-spy-edit-message)
     (define-key map (kbd "TAB") 'cider-spy-toggle-section-hidden)
     (define-key map (kbd "RET") 'cider-spy-visit-section)
     map))
