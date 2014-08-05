ad50203b32b5fdd767570a322c267e99bc6b1c09
Jon Pither <jon.pither@gmail.com>
2014-08-05 07:48:38 +0100

    1 test to go

--- test/cider-spy-tests.el
+++ test/cider-spy-tests.el
@@ -31,17 +31,6 @@
 (require 'ert)
 (require 'cider-spy)
 
-(defun cider-spy-test-grab-section (buffer k)
-  (with-current-buffer buffer
-    (car (-filter (lambda (section) (eq k (cider-spy-section-type section)))
-                  (cider-spy-descendent-sections cider-spy-root-section)))))
-
-(defun cider-spy-test-grab-section-as-string (buffer k)
-  (with-current-buffer buffer
-    (let ((section (cider-spy-test-grab-section (current-buffer) k)))
-      (buffer-substring-no-properties (cider-spy-section-beginning section)
-                                      (- (cider-spy-section-end section) 1)))))
-
 (ert-deftest test-navigate-around-sections ()
   (with-temp-buffer
     (cider-spy-refresh-buffer (current-buffer)
