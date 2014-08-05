e5406b786ed0917914625cdc180b3b1007b85702
Jon Pither <jon.pither@gmail.com>
2014-08-05 08:05:01 +0100

    Beginning of navigation test.

--- features/step-definitions/cider-spy-steps.el
+++ features/step-definitions/cider-spy-steps.el
@@ -8,3 +8,18 @@
                          "\n" "" (cider-spy-test-grab-section-as-string
                                   (get-buffer-create "buf") (make-symbol section)))))
       (assert (equal str section-text) nil "Expected %S got %S" str section-text))))
+
+(Then "^I go to beginning of summary buffer$"
+  (lambda ()
+    (with-current-buffer (get-buffer-create "buf")
+      (goto-char 1))))
+
+(And "^I go to the next section$"
+  (lambda ()
+    (with-current-buffer (get-buffer-create "buf")
+      (cider-spy-next-section (current-buffer)))))
+
+(Then "^I should be on a \"\\(.+\\)\" section$"
+  (lambda (section)
+    (with-current-buffer (get-buffer-create "buf")
+      (assert (string-equal section (cider-spy-section-type (cider-spy-find-section-at-point)))))))
