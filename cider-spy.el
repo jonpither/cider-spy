50e3983e7546f96ed47e57014228c8051c6af114
Jon Pither <jon.pither@gmail.com>
2014-08-11 14:51:12 +0100

    Merging in cider-spy into cider-spy-msg for leaness and pragmatism of deployment.

--- cider-spy.el
+++ cider-spy.el
@@ -24,7 +24,7 @@
 ;; This file is not part of GNU Emacs.
 
 (require 'cider-interaction)
-(require 'cider-spy-msg)
+(require 'nrepl-client)
 (require 'json)
 (require 'dash)
 
@@ -368,6 +368,84 @@ the current buffer will be updated accordingly."
                           ("Devs Hacking:" . font-lock-keyword-face)))
 
 (add-hook 'cider-repl-mode-hook 'cider-spy-connect-to-hub)
+
+;; cider-spy msg:
+
+(defvar cider-spy-msg-edit-buffer-name "*cider spy msg*"
+  "Buffer name for composing messages.")
+
+(defvar cider-spy-msg-popup-buffer-name-template "*hub %s*"
+  "Buffer name for message popup.")
+
+(defvar cider-spy-edit-prev-window-configuration nil)
+
+(defvar cider-spy-recipient-id nil)
+
+(defun cider-spy-msg-edit (id alias)
+  (interactive)
+  (let ((buf (get-buffer-create cider-spy-msg-edit-buffer-name)))
+    (setq cider-spy-edit-prev-window-configuration
+          (current-window-configuration))
+    (setq cider-spy-recipient-id id)
+    (pop-to-buffer buf)
+    (erase-buffer)
+    (insert (format "## Send message to %s\n\n" alias))
+    (cider-spy-edit-mode)
+    (font-lock-fontify-buffer)
+    (message "Type C-c C-c to send (C-c C-k to cancel).")))
+
+(defun cider-spy-msg-from-edit-buffer ()
+  (interactive)
+  (with-current-buffer cider-spy-msg-edit-buffer-name
+    (save-excursion
+      (goto-char 0)
+      (re-search-forward "Send.*\n\n")
+      (buffer-substring-no-properties (match-end 0) (point-max)))))
+
+(defun cider-spy-msg-send ()
+  (interactive)
+  (nrepl-send-request
+   (list "op" "cider-spy-hub-send-msg"
+         "session" (nrepl-current-session)
+         "recipient" (symbol-name cider-spy-recipient-id)
+         "message" (cider-spy-msg-from-edit-buffer))
+   nil)
+  (message "Sent message to %s." cider-spy-recipient-id)
+  (cider-spy-msg-quit))
+
+(defun cider-spy-msg-quit ()
+  (interactive)
+  (kill-buffer (get-buffer cider-spy-msg-edit-buffer-name))
+  (when cider-spy-edit-prev-window-configuration
+    (set-window-configuration cider-spy-edit-prev-window-configuration)
+    (setq cider-spy-edit-prev-window-configuration nil)))
+
+(defun cider-spy-msg-popup (from msg)
+  (with-current-buffer (get-buffer-create (format cider-spy-msg-popup-buffer-name-template from))
+    (insert from)
+    (insert " >> ")
+    (insert msg)
+    (insert "\n")
+    (cider-spy-popup-mode)
+    (font-lock-fontify-buffer))
+  (pop-to-buffer (format cider-spy-msg-popup-buffer-name-template from)))
+
+(defvar cider-spy-edit-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") 'cider-spy-msg-send)
+    (define-key map (kbd "C-c C-k") 'cider-spy-msg-quit)
+    map))
+
+(define-derived-mode cider-spy-edit-mode text-mode "Cider Spy Edit")
+
+(font-lock-add-keywords 'cider-spy-edit-mode
+                        '(("## Send.*\n" . font-lock-comment-face)))
+
+(define-derived-mode cider-spy-popup-mode text-mode "Cider Spy Popup")
+
+(font-lock-add-keywords 'cider-spy-popup-mode
+                        '(("^.*\s" . font-lock-keyword-face))
+                        '((">>" . font-lock-comment-face)))
 
 (provide 'cider-spy)
 
